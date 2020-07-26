eb73920df6357355ad650f334fb1ceab1ea696a8
Alex Florisca <alexflorisca@gmail.com>
2020-07-26 13:53:52 +0100

    Firebase comment

--- routes/generate.js
+++ routes/generate.js
@@ -7,7 +7,7 @@ module.exports = async (ctx, next) => {
 	const processCritical = fork('lib/processCriticalCss.js', ['--shop', ctx.session.shop, '--accessToken', ctx.session.accessToken]);
 	processCritical.send('start');
 	processCritical.on('message', msg => {
-		ctx.body = JSON.stringify(msg);
+		// send message to client via firebase cloud messaging
 	});
 
 	ctx.body = JSON.stringify({ status: 'pending'});
