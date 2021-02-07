845cb9a3fb364cd7e4109a49e5766a964c5649e1
Alex Florisca <alexflorisca@gmail.com>
2021-02-07 22:19:33 +0000

    Remove webworker code from frontend

--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,7 @@ class Index extends Component {
 						<TextContainer>
 							<p>This will speed up your website by generating the styles needed for the initial screen. The stylesheet loads in the background and is applied when it's ready. What this means is that users don't have to wait for styles for the entire website to be downloaded before they can view the page. This speeds up the first view experience significantly, improving user experience and potentially conversions</p>
 							<p><Button primary onClick={() => {
-								console.log("generating critical css");
-								const connection = new WebSocket('ws://7acab516689f.ngrok.io/')
-								connection.onopen = () => {
-									console.log('web socket connection open');
-									connection.send('Oh heey from client');
-									connection.onmessage = (message) => {
-										console.log(message);
-									};
-								}
-								fetch('/generate').then(res => res.json()).then(res => console.log(res));
+								fetch('/generate', { method: 'POST' }).then(res => res.json()).then(res => console.log(res));
 							}}>Generate</Button></p>
 						</TextContainer>
 					</Card>
