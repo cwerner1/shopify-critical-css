78a1d44762b70fe86220518e7584accef3412bed
Alex Florisca <alexflorisca@gmail.com>
2021-02-07 22:31:03 +0000

    Fix async error

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ class Index extends Component {
 					<Card title="Speed up your site with Critical CSS" sectioned>
 						<TextContainer>
 							<p>This will speed up your website by generating the styles needed for the initial screen. The stylesheet loads in the background and is applied when it's ready. What this means is that users don't have to wait for styles for the entire website to be downloaded before they can view the page. This speeds up the first view experience significantly, improving user experience and potentially conversions</p>
-							<p><Button primary onClick={() => {
+							<p><Button primary onClick={async () => {
 								const res = await fetch('/generate', { method: 'POST' })
 								const job = await res.json();
 								job.id && setInterval(() => {
