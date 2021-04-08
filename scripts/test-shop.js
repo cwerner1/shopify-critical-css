f2cf1f601290d1e015a992b9f7bbb92d1323ac54
Alex Florisca <alexflorisca@gmail.com>
2021-04-08 13:18:25 +0100

    Tidy up

--- scripts/test-shop.js
+++ scripts/test-shop.js
@@ -1,38 +1,13 @@
 const { generateForShop } = require("../lib/critical-css");
 const fs = require("fs");
 
-process.env.TIMEOUT = 300000;
+process.env.TIMEOUT = 1000;
+process.env.CRITICAL_CSS_CONCURRENCY = 1;
 
-const shopUrls = [
-  { type: 'index', url: 'https://twig-plants-and-pots.myshopify.com/' },
-  {
-    type: 'list-collections',
-    url: 'https://twig-plants-and-pots.myshopify.com/collections'
-  },
-  {
-    type: 'product',
-    url: 'https://twig-plants-and-pots.myshopify.com/products/lipstick-plant'
-  },
-  {
-    type: 'search',
-    url: 'https://twig-plants-and-pots.myshopify.com/search?q=Aeschynathus+Rasta'
-  },
-  {
-    type: 'page',
-    url: 'https://twig-plants-and-pots.myshopify.com/pages/christmas-order-dates-2020'
-  },
-  {
-    type: 'blog',
-    url: 'https://twig-plants-and-pots.myshopify.com/blogs/blog'
-  },
-  {
-    type: 'article',
-    url: 'https://twig-plants-and-pots.myshopify.com/blogs/blog/my-first-shop-in-bristol'
-  }
-]
+const shopUrls = [];
 
 generateForShop(
-	'twig-plants-and-pots.myshopify.com',
+	'',
 	{ progress: () => null }, 
 	criticalCSs => {
 		fs.writeFileSync('test.txt', criticalCSs)
